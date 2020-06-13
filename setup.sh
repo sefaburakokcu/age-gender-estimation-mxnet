c28376c203935fec16b524f2181140f3c5ed51d5
Sefa Burak OKCU <sefaburak.okcu@gmail.com>
2020-06-14 02:41:21 +0300

    Add setup file for Heroku

---
+++ setup.sh
@@ -0,0 +1,14 @@
+  
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"sefaburak.okcu@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
